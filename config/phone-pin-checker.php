889042034e53ea4dd36aeae5de65300791101a0d
Stephan van der Horn <stephan@sandwave.io>
2020-01-22 10:16:21 +0100

    Added route group with middlware config

--- config/phone-pin-checker.php
+++ config/phone-pin-checker.php
@@ -4,5 +4,8 @@ return [
 
     // Codes will stay valid for this long after creating (seconds)
     'expire' => 3600,
+
+    // Optional middlware
+    'middleware' => []
     
 ];
--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::post('phone-pin-checker', 'Sandwave\PhonePinChecker\Http\Controllers\PhonePinCheckerController@create')
-    ->name('phone-pin-checker.create');
+Route::group(['middlware' => config('phone-pin-checker.middleware')], function () {
+    Route::post('phone-pin-checker', 'Sandwave\PhonePinChecker\Http\Controllers\PhonePinCheckerController@create')
+        ->name('phone-pin-checker.create');
 
-Route::get('phone-pin-checker/{code}', 'Sandwave\PhonePinChecker\Http\Controllers\PhonePinCheckerController@check')
-    ->name('phone-pin-checker.check');
+    Route::get('phone-pin-checker/{code}', 'Sandwave\PhonePinChecker\Http\Controllers\PhonePinCheckerController@check')
+        ->name('phone-pin-checker.check');
+});
